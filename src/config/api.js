c312b5422886f5110f5c42179fb640ae63df864c
katharinefernandes <katharinefv@gmail.com>
2025-09-09 20:57:03 -0300

    feat: configura metodos rest para chamada de endpoints

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+export const apiUrl = process.env.REACT_APP_API_URL;
+
+export const POST = 'POST';
+export const PUT = 'PUT';
+export const GET = 'GET';
+export const PATCH = 'PATCH';
+export const DELETE = 'DELETE';
